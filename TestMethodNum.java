cad63ee0474b8775d2b61405900ee4ebdb2c2999
Austin Harshberger <aharshberger@sf-m-aharshberger.ideo.com>
2017-04-18 13:51:28 -0700

    Adding practive files to github so I have them to reference later

---
+++ TestMethodNum.java
@@ -0,0 +1,17 @@
+class TestMethodNum{
+
+public static int addingNum(int a, int b, int c){
+
+int addedNumbers = a + b * c;
+
+return addedNumbers;
+
+}
+
+public static void main(String [] args){
+
+System.out.println(addingNum(7, 8, 10));
+
+}
+
+}
---
+++ TestMethods.java
@@ -0,0 +1,11 @@
+class TestMethods{
+public static String concat(String a, String b){
+String f = a + b;
+return f;
+}
+public static void main(String [] args){
+System.out.println(concat("Austin", "Harshberger"));
+}
+}
+
+
